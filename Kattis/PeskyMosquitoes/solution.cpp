0e6206411d392dd97a9aedf9f0e2e5ca74785a47
Eric Ge <ericlege@berkeley.edu>
2018-11-06 23:45:14 -0800

    practice

--- Kattis/PeskyMosquitoes/solution.cpp
+++ Kattis/PeskyMosquitoes/solution.cpp
@@ -7,9 +7,6 @@ int contained(double centerx, double centery,double x,double y,double r){
   return pow(r, 2)+ 0.000001 >= pow(centerx-x, 2) + pow(centery-y, 2);
 }
 
-
-
-
 int main(){
   int l, m, res, count1, count2;
   double X[32], Y[32], centerx1, centery1, centerx2, centery2, r, q, x1, x2, y1, y2, x3, y3;
@@ -25,10 +22,8 @@ int main(){
     }
     for (int i=0;i<m;i++){
       for (int j=i+1;j<m;j++){
-
         count1 = 0;
         count2 = 0;
-
         x1 = X[i];
         y1 = Y[i];
         x2 = X[j];
@@ -39,7 +34,6 @@ int main(){
     	  x3 = (x1+x2)/2;
     		basex = sqrt(pow(r,2)-pow((q/2),2))*(y1-y2)/q; //calculate once
     	  basey = sqrt(pow(r,2)-pow((q/2),2))*(x2-x1)/q; //calculate once
-
     		centerx1 = x3 + basex; //center x of circle 1
     		centery1 = y3 + basey; //center y of circle 1
     		centerx2 = x3 - basex; //center x of circle 2
@@ -55,3 +49,4 @@ int main(){
     cout << res << endl;
   }
 }
+
