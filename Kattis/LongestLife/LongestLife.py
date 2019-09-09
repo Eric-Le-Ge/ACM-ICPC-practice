9c599f4bc5f6e74fe01dd0387d466e39337ea0e2
Eric Ge <ericlege@berkeley.edu>
2019-09-08 22:37:11 -0700

    fixed ll

--- Kattis/LongestLife/LongestLife.py
+++ Kattis/LongestLife/LongestLife.py
@@ -1,3 +1,4 @@
+from collections import deque
 n, p, c = [int(_) for _ in input().split()]
 ps = [(0, 1.0)]
 for _ in range(p):
@@ -5,27 +6,32 @@ for _ in range(p):
 	q = x/y
 	ps.append((t, q))
 inf = int(10e24)
-dq = [[0, 1.0, 0, inf]]
+# index, q, start, end, t, height
+dq = deque([[0, 1.0, 0, inf, 0, 0]])
+acc = deque()
 for i in range(1, len(ps)):
 	t, q = ps[i]
 	if q <= dq[-1][1]:
 		continue
-	# while dq and dq[0][3] < t:
-	# 	dq.pop(0)
+	while dq[0][3] < t:
+		acc.append(dq.popleft())
+	height = dq[0][5] + (t-dq[0][4])/dq[0][1] + c
 	overtake = 0
 	while dq:
-		overtake = t + (c*dq[-1][1]*q)/(q-dq[-1][1])
+		heightdiff = height - (dq[-1][5] + (t-dq[-1][4])/dq[-1][1])
+		overtake = t + (heightdiff*dq[-1][1]*q)/(q-dq[-1][1])
 		if overtake > dq[-1][2]:
 			break
 		dq.pop()
 	if dq:
 		dq[-1][3] = overtake
-	dq.append([i, q, overtake, inf])
+	dq.append([i, q, overtake, inf, t, height])
 res = 0.0
+acc.extend(dq)
 n = float(n)
 while n>0:
-	cost = min(n, (dq[0][3]-dq[0][2])/dq[0][1])
-	res += cost*dq[0][1]
+	cost = min(n, (acc[0][3]-acc[0][2])/acc[0][1])
+	res += cost*acc[0][1]
 	n -= cost
-	dq.pop(0)
+	acc.popleft()
 print(res)
