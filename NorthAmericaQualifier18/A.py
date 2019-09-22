1e6f91b073db6e66806e679962bced9146a80c91
Eric Ge <ericlege@berkeley.edu>
2019-09-21 23:20:14 -0700

    practice naq17

--- NorthAmericaQualifier18/A.py
+++ NorthAmericaQualifier18/A.py
@@ -1,69 +1,45 @@
-import sys
-n = int(sys.stdin.readline())
-elems = {}
-inputs = []
-m =0
-res = "no tie"
-win = False
-for line in sys.stdin:
-	line = line[:-2]
-	if len(line) < 5:
-		continue
-	elem =  [int(e) for e in line.split(' ')]
-	inputs.append(elem)
-
-def check(a,b):
-	match = 0
-	used = [False, False, False, False, False]
-	for x in range(5):
-		for y in range(x,5):
-			if a[x] == b[y] and not used[y]:
-				used[y] = True
-				match += 1
-				break
-	return match == 5
-
+n = int(raw_input())
+from collections import defaultdict
+adds = defaultdict(list)
+cnts = [0]*500
+rows = []
 for i in range(n):
-	if win:
-		break
-	start_line = i*5
-	a0 = []
-	a1 = []
-	a2 = []
-	a3 = []
-	a4 = []
-	for j in range(5):
-		a0.append(inputs[start_line+j][0])
-		a1.append(inputs[start_line+j][1])
-		a2.append(inputs[start_line+j][2])
-		a3.append(inputs[start_line+j][3])
-		a4.append(inputs[start_line+j][4])
-		sum_row = sum(inputs[start_line+j])
-		if sum_row in elems:
-			for l in elems[sum_row]:
-				if check(l) and not win:
-					win = True
-					res = (l[5]+1) + " " + (i+1)
-	for col in [a0,a1,a2,a3,a4]:
-		sum_col = sum(col)
-		if sum_col in elems:
-			for c in elems[sum_col]:
-				if check(c) and not win:
-					win = True
-					res = (c[5]+1) + " " + (i+1)
-	for j in range(5):
-		sum_row = sum(inputs[start_line+j])
-		inputs[start_line+j].append(i)
-		if sum_row in elems:
-			elems[sum_row].append(inputs[start_line+j])
-		else:
-			elems[sum_row] = [inputs[start_line+j]]
-	for col in [a0,a1,a2,a3,a4]:
-		sum_col = sum(col)
-		col.append(i)
-		if sum_col in elems:
-			elems[sum_col].append(col)
-		else:
-			elems[sum_col] = [col]
-print(elems)
-print res
+	for r in range(5):
+		row = [int(_) for _ in raw_input().split()]
+		rows.append(set(row))
+		for e in row:
+			adds[e].append(i*5+r)
+	if i < n-1:
+		raw_input()
+
+def inject(un, sec):
+	for e in un - sec:
+		for target in adds[e]:
+			cnts[target]+=1
+			if cnts[target]==5:
+				return False
+	for j in list(sec):
+		win = True
+		sec.remove(j)
+		for k in sec:
+			for target in adds[k]:
+				cnts[target]+=1
+				if cnts[target]==5:
+					win = False
+		if win:
+			return True
+		for k in sec:
+			for target in adds[k]:
+				cnts[target]-=1
+		sec.add(j)
+	return False
+
+for i in range(n*5-1):
+	for j in range((i//5+1)*5, n*5):
+		un, sec = rows[i] | rows[j], rows[i] & rows[j]
+		cnts = [0]*500
+		if inject(un, sec):
+			print (str(i//5 + 1)+' '+str(j//5+1))
+			exit()
+print('no ties')
+
